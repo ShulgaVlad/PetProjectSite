66e8a955a85b506f1261c4fdb2f6b425d64acd88
Rvkin <vladikkotlyar28092004@gmail.com>
2024-01-23 13:55:20 +0200

    v-2

--- Site/PetProjSite/PetProjSite/obj/Debug/net6.0/PetProjSite.AssemblyInfo.cs
+++ Site/PetProjSite/PetProjSite/obj/Debug/net6.0/PetProjSite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetProjSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7deb6da89a6fbadd82700d4c27779d46515330c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetProjSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetProjSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
